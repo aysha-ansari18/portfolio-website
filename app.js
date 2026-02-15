0a76e01444d732bcdb81d09a49173d5654d0ac87
aysha-ansari18 <ansariaysha909@gmail.com>
2026-02-15 12:40:00 +0530

    Added root route

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.set("view engine","ejs" );
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"/public")));
 
+app.get("/", (req, res) => {
+  res.redirect("/home");
+});
+
+
 app.get('/home', (req, res) => {
   res.render('pages/index');
 });
